bbcf3606799cc3203e0d8b6737b6f6ec82494960
ge0var <jovial700@gmail.com>
2026-09-24 23:37:34 -0500

    not yet working. But making progress

--- 01-bubble-sort/Main.java
+++ 01-bubble-sort/Main.java
@@ -1,9 +1,22 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-        int[] unsortedArray = {10, 20, 50, 43, 69, 1};
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Enter comma separated list of integers to form unsorted input array.");
+        String inputString = scanner.nextLine();
+
+        String[] inputArrayString = inputString.split(",\\s*");
+
+        int[] unsortedArray = new int[inputArrayString.length]; 
+
+        for (int i=0; i<inputArrayString.length; i++) {
+            unsortedArray[i] = Integer.parseInt(inputArrayString[i].trim());
+        }
+
         int[] sortedArray = BubbleSort.getSortedArray(unsortedArray);
 
         System.out.println(Arrays.toString(sortedArray)); 
@@ -12,7 +25,17 @@ public class Main {
 
 class BubbleSort {
     public static int[] getSortedArray(int[] unsortedArray) {
-        int[] sortedArray = unsortedArray;
-        return sortedArray;
+        int temp;
+        for (int i=0; i < unsortedArray.length; i++) {
+            if (i == unsortedArray.length - 1) {
+                return unsortedArray;
+            }
+            if (unsortedArray[i] > unsortedArray[i+1]) {
+                temp = unsortedArray[i];
+                unsortedArray[i] = unsortedArray[i+1];
+                unsortedArray[i+1] = temp;
+            }
+        }
+        return unsortedArray;
     }
 }
